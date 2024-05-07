a4665680be1cf888a5ec3277f0f9d7f08efd5ca8
kelvinvaghasiya2002 <kelvinvaghasiya50@gmail.com>
2024-05-07 13:36:43 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const client = process.env.CLIENT_URL;
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, email");
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
